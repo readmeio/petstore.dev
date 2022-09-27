7e432f4d67c7cdf0e0614cd2578bef71a880ec1c
gkoberger <gkoberger@gmail.com>
2022-09-27 00:29:02 -0400

    pointer

--- pages/index.js
+++ pages/index.js
@@ -184,7 +184,7 @@ export default function Home({ files }) {
                               tab.version == version.version
                                 ? "border-indigo-500 text-indigo-600"
                                 : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
-                              "w-1/3 py-4 px-1 text-center border-b-2 font-medium text-sm"
+                              "w-1/3 py-4 px-1 text-center border-b-2 font-medium text-sm cursor-pointer"
                             )}
                           >
                             {tab.name}
